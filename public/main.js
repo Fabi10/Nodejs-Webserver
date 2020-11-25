852a7b36ecd335b835965109f49d92d92836f2e7
Fabi10 <fabiuder@gmx.de>
2020-11-25 20:30:19 +0100

    new

--- public/main.js
+++ public/main.js
@@ -1,16 +1,33 @@
 
 // Map initialisieren 
 function init() {
-    var map = L.map('map').setView([49.755, 6.639], 141);
+    var map = L.map('map').setView([49.755, 6.639], 141); //Trier Koordinaten
     // add the OpenStreetMap tiles
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 16,
+        maxZoom: 19,
         attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>'
+
     }).addTo(map);
 
     // show the scale bar on the lower left corner
     L.control.scale().addTo(map);
 
     // show a marker on the map
-    L.marker({lon: 0, lat: 0}).bindPopup('The center of the world').addTo(map);
+    const marker = L.marker([49.755, 6.639]).addTo(map);
+
+    //map.setView() --> Map zentrieren
+
+
+
 }
+   
+
+
+
+
+
+
+
+
+//GET - fordert Daten vom Server an
+//POST - übermittelt Daten an den Server
